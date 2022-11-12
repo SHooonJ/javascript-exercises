1b20302aaac171605ddcdeef08bc75e8369df9cb
SHoonJ <andrewstudy629@gmail.com>
2022-11-12 00:25:39 -0800

    Pass all test, but could be sub-optimal

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...numArg) {
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < numArg.length; j++){
+            if(arr[i] === numArg[j]){
+                arr[i] = 'rm';
+            }
+        }
+    }
+    let ans = [];
+    for(const x of arr){
+        if(x !== 'rm'){
+            ans.push(x);
+        }
+    }
+    return ans;
 };
 
 // Do not edit below this line
