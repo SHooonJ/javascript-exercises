78469480debedecf60b9979ec43fde8b2fe74aff
SHoonJ <andrewstudy629@gmail.com>
2022-11-12 00:43:57 -0800

    Pass test using .include and .forEach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,14 @@
 const removeFromArray = function(arr, ...numArg) {
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < numArg.length; j++){
-            if(arr[i] === numArg[j]){
-                arr[i] = 'rm';
-            }
-        }
-    }
     let ans = [];
-    for(const x of arr){
-        if(x !== 'rm'){
-            ans.push(x);
+    arr.forEach((element) => {
+        if(!numArg.includes(element)){
+            ans.push(element);
         }
-    }
+    });
+
     return ans;
+
+
 };
 
 // Do not edit below this line
