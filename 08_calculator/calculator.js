7c8b5a325ab482afa03b6536f70f575772a8dd1c
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 14:15:24 -0800

    Pass test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let ans = 0;
+  numArr.forEach(element => {ans += element;});
+  return ans;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let ans = 1;
+  numArr.forEach(element => {ans *= element;});
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	return base**power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0){return 1;}
+  let ans = 1;
+  for(let i = 1; i <= num; i++){
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
