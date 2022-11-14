981a4252edaf3dbeb4500353def1772763f62cfc
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 14:53:47 -0800

    Pass using .replace and regExp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,11 +1,6 @@
 const palindromes = function (str) {
-    let fixedStr = '';
-    str = str.toLowerCase();
-    for(let i = 0; i < str.length; i++){
-        let code = str.charCodeAt(i);
-        if(!(code <= 122 && code >= 97)){continue;}
-        fixedStr += str[i];
-    }
+    let fixedStr = str.toLowerCase().replace(/[^a-z]/g,"");
+
 
     for(let i = 0; i <= Math.floor(fixedStr.length/2); i++){
         if(fixedStr[i] !== fixedStr[fixedStr.length - i - 1]){return false;}
