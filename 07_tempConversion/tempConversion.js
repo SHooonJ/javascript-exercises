2cdbf5f7dee8af7b27c1ff4244508c0d761777d8
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 13:51:38 -0800

    Pass test

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+  let celsius = (degree - 32) * (5/9);
+  celsius = Math.round(celsius * 10)/10;
+  return celsius;
 };
 
-const ctof = function() {
-
+const ctof = function(degree) {
+  let farenheit = (degree * (9/5)) + 32;
+  farenheit = Math.round(farenheit * 10)/10;
+  return farenheit;
 };
 
 // Do not edit below this line
