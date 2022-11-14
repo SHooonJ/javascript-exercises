0deecd79fa8a9774d05c21c8c0745455265bdceb
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 13:41:12 -0800

    Pass test

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let four = (year % 4 === 0);
+    let hundred = (year % 100 === 0);
+    let fourHundred = (year % 400 === 0);
 
+    if(fourHundred === true){return true;}
+    else if(hundred === true){return false;}
+    else if(four === true){return true;}
+    else{return false;}
+    
 };
 
 // Do not edit below this line
