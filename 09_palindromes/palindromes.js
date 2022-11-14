9b1f514a5fb26c517d75d96be30cf8e692072ae8
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 14:40:59 -0800

    Pass test using .toLowerCase and .charCodeAt

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let fixedStr = '';
+    str = str.toLowerCase();
+    for(let i = 0; i < str.length; i++){
+        let code = str.charCodeAt(i);
+        if(!(code <= 122 && code >= 97)){continue;}
+        fixedStr += str[i];
+    }
 
+    for(let i = 0; i <= Math.floor(fixedStr.length/2); i++){
+        if(fixedStr[i] !== fixedStr[fixedStr.length - i - 1]){return false;}
+    }
+    return true;
 };
 
 // Do not edit below this line
