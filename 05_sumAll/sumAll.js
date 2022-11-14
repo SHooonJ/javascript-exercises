8ad68da8e7479efa8416b60e8e96ecb8d7338ec9
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 13:26:04 -0800

    Pass test using brute force

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if(Number.isInteger(num1) === false || Number.isInteger(num2) === false
+    || num1 < 0 || num2 < 0){
+        return 'ERROR';
+    }
+    let ans = 0;
+    let smallNum = 0, largeNum = 0;
+    if(num1 < num2){
+        smallNum = num1;
+        largeNum = num2;
+    }
+    else{
+        smallNum = num2;
+        largeNum = num1;
+    }
+    for(let i = smallNum; i <= largeNum; i++){
+        ans += i;
+    }
+    return ans;
+
 
 };
 
