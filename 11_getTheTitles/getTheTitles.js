4a0f7575c5fb3b930451ebdaaf4f1d5142e02a08
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 16:04:03 -0800

    Pass test creating a second function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+function test(element){
+    return element.title;
+}
 
+const getTheTitles = function(books) {
+    let ans = books.map(test);
+    return ans;
 };
 
 // Do not edit below this line
