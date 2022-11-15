4cf3d5e69506715bd7f63d7359e6ff8fc446967f
SHoonJ <andrewstudy629@gmail.com>
2022-11-14 16:44:43 -0800

    Pass test using .reduce and creating getAge function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(person){
+    if(!person.yearOfDeath){
+        today = new Date();
+        death = today.getFullYear();
+    }
+    else{
+        death = person.yearOfDeath;
+    }
+    return death - person.yearOfBirth;
+}
 
+const findTheOldest = function(arr) {
+    let ans = arr.reduce((acc, curr)=>{
+        if(getAge(curr) > getAge(acc)){return curr;}
+        else{return acc};
+    },arr[0])
+    return ans;
 };
 
 // Do not edit below this line
